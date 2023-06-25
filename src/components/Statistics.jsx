5f724685c7a0da9cabfea53d08478265611f6243
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2023-06-26 02:48:31 +0300

    CSS ready

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -10,7 +10,7 @@ const Statistics = ({ title, stats }) => {
 <ul className="stat-list">
         {stats.map(({ id, label, percentage }) => (
           <li key={id} className="item">
-            <span className="label">{label}</span>
+            <span className="item_label">{label}</span>
              <span className="percentage">{percentage}%</span>
           </li>
         ))}
